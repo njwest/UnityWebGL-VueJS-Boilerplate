72322c100229a84618b2aa82da6e81a92c5bb1b6
njwest <njwest9@gmail.com>
2019-04-23 22:40:23 +0800

    moar docs + comments

--- README.md
+++ README.md
@@ -1,11 +1,24 @@
-# Unity Vue Boilerplate
+# UnityWebGL VueJS Boilerplate
 
 This is a boilerplate **Unity WebGL** ++ **VueJS** web app with **VueX && **vue-router** pre-installed.
 
 This project takes advantage of the Unity3d web container wrapper component provided by
 @votetake's [vue-unity-webgl](https://github.com/votetake/vue-unity-webgl) Node package.
 
+## Directory Structure
 
+`./public/Build` && `./public/TemplateData` - the templates/JS functions of a bare Unity3D WebGL project.
+
+## Send a string from VueJS to the Unity Scene
+
+The Unity scene contains only a lean 3D plane and a text object.
+
+Text can be sent to the text object in the Unity game instance via the `@click` method in `./src/views/Home.vue`:
+
+```JavaScript
+// $refs.myInstance === <unity ref="myInstance">
+this.$refs.myInstance.message('Text', 'SetText', this.textInput)
+```
 
 ## Project setup
 ```
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="">
-    <unity src="/Build/WebGL-Test.json" width="1000" height="600" unityLoader="/Build/UnityLoader.js" ref="myInstance"></unity>
+    <!--
+    The Unity Container.
+    Look in the src of npm-modules/vue-unity/webgl/ after npm installing for the mechanics of this custom component
+    -->
+    <unity
+      src="/Build/WebGL-Test.json"
+      width="1000"
+      height="600"
+      unityLoader="/Build/UnityLoader.js" ref="myInstance"
+    >
+    </unity>
+
+    <!-- Unstyled text input + button -->
     <input type="text" v-model="textInput" description="Input text here to send to Unity">
-
-    <button @click="inputText">My Button >:(</button>
+    <button @click="inputText">
+      Send a string to Unity
+    </button>
   </div>
-
 </template>
 <script>
 import Unity from 'vue-unity-webgl'
@@ -21,7 +33,7 @@ export default {
     }
   },
   methods: {
-    sendText () {
+    sendText() {
       this.$refs.myInstance.message('Text', 'SetText', this.textInput)
     }
   }
